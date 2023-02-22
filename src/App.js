706d387dec4a4420c1b53f17f77a50562bd5483b
HammadAli01 <hammadalibu@gmail.com>
2023-02-22 14:49:56 +0500

    first crud commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import tableColumn from "./data";
+import { useState, useEffect } from "react";
+let newId = 3;
 function App() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "Hammad",
+      address: "banigala",
+      email: "hammadalibu@gmail.com",
+      contact: "03045360339",
+    },
+    {
+      id: 2,
+      name: "umer",
+      address: "Rawalpindi",
+      email: "umer@gmail.com",
+      contact: "12345678911",
+    },
+    {
+      id: 3,
+      name: "saad",
+      address: "Karachi",
+      email: "saad@gmail.com",
+      contact: "5432678543",
+    },
+  ]);
+  const initialNewData = {
+    id: newId++,
+    name: "",
+    address: "",
+    contact: "",
+    email: "",
+    exist: false,
+  };
+  const [newData, setNewData] = useState(initialNewData);
+  const handleDelete = (userId) => {
+    setData(data.filter((person) => person.id !== userId));
+  };
+  const handleInputChange = (e) => {
+    setNewData({ ...newData, [e.target.name]: e.target.value });
+  };
+  const handleSubmitButton = () => {
+    console.log("newdata is =>", newData);
+    if (newData.exist) {
+      setData(data.map((user) => (user.id === newData.id ? newData : user)));
+      setNewData(initialNewData);
+    } else {
+      setData([...data, newData]);
+      setNewData(initialNewData);
+    }
+  };
+  const handleEdit = (user) => {
+    setNewData({ ...user, ["exist"]: true });
+  };
+  useEffect(() => {
+    console.log("data changed", data);
+  }, [data]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="input-group">
+        <input
+          name="name"
+          className="input-field"
+          placeholder="Enter name"
+          value={newData.name}
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+        />
+        <input
+          name="address"
+          className="input-field"
+          placeholder="Enter Address"
+          value={newData.address}
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+        />
+        <input
+          name="email"
+          type="email"
+          className="input-field"
+          placeholder="Enter Email"
+          value={newData.email}
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+        />
+        <input
+          name="contact"
+          type="tel"
+          className="input-field"
+          placeholder="Enter Contact"
+          value={newData.contact}
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+        />
+        <button
+          onClick={() => {
+            handleSubmitButton();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </div>
+      <table>
+        <thead>
+              
+          <tr>
+               
+            {tableColumn.map((heading) => {
+              return <th>{heading.columnName}</th>;
+            })}
+               
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user["id"]}</td>
+                <td>{user["name"]}</td>
+                <td>{user["email"]}</td>
+                <td>{user["contact"]}</td>
+                <td>{user["address"]}</td>
+                <td>{user["action"]}</td>
+                <td>
+                  <button
+                    className="action-button"
+                    onClick={() => {
+                      handleEdit(user);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="action-button"
+                    onClick={() => {
+                      handleDelete(user.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+                        
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
